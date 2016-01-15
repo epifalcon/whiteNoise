dd96015d8877832416513dbf26fc3a7d0467601d
epifalcon <kyleshepherdson@gmail.com>
2016-01-15 18:20:51 +0000

    Fixed unintended (non-audio) white noise during pause. Inefficient, but functional.

--- white_noise.py
+++ white_noise.py
@@ -10,12 +10,7 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def main():
-    pygame.init()
-    # Load the sound to play indefinitely
-    pygame.mixer.music.load('white_noise.ogg')
-    pygame.mixer.music.play(-1)
     # Pause the sound
-    pygame.mixer.music.pause()
     paused = True
 
     # Poll button and toggle pausing
@@ -23,15 +18,18 @@ def main():
         input_state = GPIO.input(18)
         if input_state == False:
             if paused:
-                pygame.mixer.music.unpause()
+                pygame.init()
+                # Load the sound to play indefinitely
+                pygame.mixer.music.load('white_noise.ogg')
+                pygame.mixer.music.play(-1)
                 paused = False
                 time.sleep(1)
-                print paused
+                print 'playing'
             else:
-                pygame.mixer.music.pause()
+                pygame.quit()
                 paused = True
                 time.sleep(1)
-                print paused
+                print 'paused'
 
 
 if __name__ == '__main__':
